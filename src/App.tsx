2ddeae1df93d6a50a50b4104eeb82dcaa5667528
Majid Gabrlö <majidgabrlo@gmail.com>
2022-08-05 21:15:52 +0330

    add material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import translator from "./utils/translator";
 import latestNewsGetter from "./utils/latestNewsGetter";
+
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
